593dc2adc94e439d80d9182086082728ca9ca9fb
koya <azumabass@gmail.com>
2023-03-30 22:42:14 +0900

    スナップショットテストを追加

--- src/SimpleButton.test.tsx
+++ src/SimpleButton.test.tsx
@@ -15,3 +15,11 @@ test("ボタンをクリックするとON/OFFの表示が切り替わる", () =>
     // クリックしてONになったかどうかをテスト
     expect(simpleButton).toHaveTextContent("ON");
 });
+
+test("描画されてすぐはOFFと表示されている", () => {
+    const view = render(<SimpleButton />);
+    // 描画したコンポーネントとスナップショットを照合する
+    //    → コンポーネントのDOM と、描画された DOM を照合する
+    //    → __snapshots__ に保存され、2回目以降、これと比較される。
+    expect(view.container).toMatchSnapshot();
+});
